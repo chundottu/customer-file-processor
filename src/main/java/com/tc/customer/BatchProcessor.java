8c23c83d29792a8040f9bf69ff21dfa4a094c4e2
chundottu <sam_chundottu@yahoo.com>
2016-10-11 21:47:06 -0400

    First commit

---
+++ src/main/java/com/tc/customer/BatchProcessor.java
@@ -0,0 +1,11 @@
+package com.tc.customer;
+
+/**
+ * Created by sam on 10/11/2016.
+ */
+public class BatchProcessor {
+    public static void main(String args[])
+    {
+        System.out.println("In BatchProcessor");
+    }
+}
